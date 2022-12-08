5c197c492a053e89f40ecd77221d2a187525efd9
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-12-08 09:18:35 -0700

    importing files to app.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import css
+import "./App.css";
+//apollo client
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+//import components
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Page from "./components/Page";
+import PageContent from "./components/PageContent";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
