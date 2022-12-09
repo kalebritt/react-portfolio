72bb0f56b423f078670f2d1c9060cf62fdd5560b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-12-09 07:39:48 -0700

    npm run build, working on aboutjs

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,33 +4,32 @@ import "./about.css";
 function About() {
   return (
     <div className="container home">
-      <div className="">
-      </div>
+      <div className=""></div>
 
       <div className="info">
-        <h2 className="heading">Full Stack Web Developer</h2>
+        <h2 className="heading">poet/full.stack.developer</h2>
         <p>
-          Full Stack Web Developer with a background in business ownership,
+          {/* Full Stack Web Developer with a background in business ownership,
           massage therapy, and life-long dedication to learning. Self-starter
           with excellent communication and customer service skills. Known among
           former clients for being able to listen to their needs and delivering
           a consistently excellent product. Mentor a new massage therapist to
           coach on advanced massage therapy techniques, business ownership, and
-          cultivating an interpersonal relationship with each client.
-          </p>
-          <p>
+          cultivating an interpersonal relationship with each client. */}
+        </p>
+        <p>
           Extra-curricular activities include being MVP of the roller derby
-          league, training committee member, team captain, and assists with coaching
-          the new recruits. Love collaborating as a team to reach common goals
-          and help everyone achieve success.
-          </p>
-          <p>
-          I thrive on witnessing and helping everyone around me succeed
-          and being able to contribute to that success.
+          league, training committee member, team captain, and assists with
+          coaching the new recruits. Love collaborating as a team to reach
+          common goals and help everyone achieve success.
+        </p>
+        <p>
+          I thrive on witnessing and helping everyone around me succeed and
+          being able to contribute to that success.
         </p>
       </div>
     </div>
   );
 }
 
-export default About
+export default About;
