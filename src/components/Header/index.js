cf7668a76c831845215fe501c3415c7a58b13123
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-12-09 12:15:12 -0700

    headerjs

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './header.css';
+
+function Header ({ children }) {
+  return (
+    <div >
+      <header className='title'>
+        <h1>k</h1>
+      {children}
+      </header>
+    </div>
+ 
+  );
+}
+
+export default Header;
